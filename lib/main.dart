c85fcaeccc6cd67106efa1852591831830e8a93f
AtharvC <atharvchakole370@gmail.com>
2022-05-30 20:50:27 -0400

    Bottom Navigation Bar + Placeholder buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,43 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MainPage());
+}
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  _MainPageState createState() => _MainPageState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class _MainPageState extends State<MainPage> {
+  int currentIndex = 0;
+  final screens = [
+    Center(
+      child: TextButton(
+        onPressed: () {},
+        child: const Text('Home'),
+      ),
+    ),
+    Center(
+      child: TextButton(
+        onPressed: () {},
+        child: const Text('My Chapter'),
+      ),
+    ),
+    Center(
+      child: TextButton(
+        onPressed: () {},
+        child: const Text('Requests'),
+      ),
+    ),
+    Center(
+      child: TextButton(
+        onPressed: () {},
+        child: const Text('Account'),
+      ),
+    ),
+  ];
 
   // This widget is the root of your application.
   @override
@@ -13,13 +45,35 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.blueAccent,
           title: const Text('Lean On Us App'),
         ),
-        body: Container(
-          child: const Text('Demo for LoU'),
-          margin: const EdgeInsets.all(10),
-          padding: const EdgeInsets.all(10),
+        body: screens[currentIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Colors.blueAccent,
+          currentIndex: currentIndex,
+          onTap: (index) => setState(() => currentIndex = index),
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
+                backgroundColor: Colors.blue,
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.business),
+                label: 'My Chapter',
+                backgroundColor: Colors.blue,
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.location_on_sharp),
+                label: 'Requests',
+                backgroundColor: Colors.blue,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle_rounded),
+              label: 'Account',
+              backgroundColor: Colors.blue,
+            ),
+          ],
         ),
       ),
     );
